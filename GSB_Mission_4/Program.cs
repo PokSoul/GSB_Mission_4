fe25b8324626052651c532d140ab561d1a48489a
Poksoul <saiyudmeechai@gmail.com>
2019-03-14 13:51:11 +0100

    Ajout class Connexion

--- GSB_Mission_4/Program.cs
+++ GSB_Mission_4/Program.cs
@@ -17,6 +17,7 @@ namespace GSB_Mission_4
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
