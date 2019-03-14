d1bacd79c9de549ff3af057da886c95d972cabd7
Maxime <maxime.dubost.75@gmail.com>
2019-03-14 14:46:09 +0100

    Méthodes de gestion de DateTime

---
+++ GSB_Mission_4/GestionDate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB_Mission_4
+{
+    class GestionDate
+    {
+        public string getCurrentMonth(DateTime date)
+        {
+            return date.Month.ToString();
+        }
+
+        public string getPreviousMonth(DateTime date)
+        {
+            var previousMonth = date.Month - 1;
+            return previousMonth.ToString();
+        }
+
+        public string getNextMonth(DateTime date)
+        {
+            var nextMonth = date.Month + 1;
+            return nextMonth.ToString();
+        }
+    }
+}
